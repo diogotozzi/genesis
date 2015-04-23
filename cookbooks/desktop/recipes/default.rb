297d20ff2e96ef7de67b54ed0863c0db54b1c987
Diogo Tozzi <tozzi.diogo@gmail.com>
2015-04-23 17:53:44 -0300

    Added configuration and packages to be installed

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -4,3 +4,51 @@
 #
 # Copyright 2015, Your Name
 #
+
+packages = [
+  # Basic packages
+  "build-essential",
+  "curl",
+  "dkms",
+  "git",
+  "htop",
+  "nfs-common",
+  "nfs-kernel-server",
+  "python-pip",
+  "sl",
+  "unzip",
+  "vim",
+  "zip",
+  # Media packages
+  "ubuntu-restricted-extras",
+  "vlc"
+]
+
+packages.each do |pack|
+  package pack
+end
+
+# AWS configuration
+template "#{ENV['HOME']}/.aws/config" do
+  source "config.erb"
+end
+
+template "#{ENV['HOME']}/.aws/credentials" do
+  source "credentials.erb"
+end
+
+execute "Install AWS-Cli" do
+  command "pip install awscli"
+  action :run
+end
+
+# Git configuration
+template "#{ENV['HOME']}/.gitconfig" do
+  source "gitconfig.erb"
+end
+
+# Ruby Gems
+execute "Install Gems" do
+  command "gem install --no-ri --no-rdoc bundler capistrano capistrano-ec2_tagged capistrano-symfony chef knife-ec2"
+  action :run
+end
