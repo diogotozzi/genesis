81b35e4dbadadbaf8f544b721cc92cc410039404
Diogo Tozzi <tozzi.diogo@gmail.com>
2015-11-03 18:49:17 -0200

    Fixed coding standard

--- cookbooks/desktop/attributes/default.rb
+++ cookbooks/desktop/attributes/default.rb
@@ -1,16 +1,16 @@
 default["user"] = ""
 default["group"] = ""
-default['rvm']['user_installs'] = [
-  { 'user'          => '',
-    'default_ruby'  => '2.2.1',
-    'rubies'        => []
+default["rvm"]["user_installs"] = [
+  { "user" => "",
+    "default_ruby" => "2.2.1",
+    "rubies" => []
   }
 ]
-default['rvm']['user_global_gems'] = [
-  { 'name' => 'bundler'},
-  { 'name' => 'capistrano'},
-  { 'name' => 'capistrano-ec2_tagged'},
-  { 'name' => 'capistrano-symfony'},
-  { 'name' => 'chef'},
-  { 'name' => 'knife-ec2'}
+default["rvm"]["user_global_gems"] = [
+  { "name" => "bundler"},
+  { "name" => "capistrano"},
+  { "name" => "capistrano-ec2_tagged"},
+  { "name" => "capistrano-symfony"},
+  { "name" => "chef"},
+  { "name" => "knife-ec2"}
 ]
